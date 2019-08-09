fbcdf4778b8badd16edbc398a9fa0213792187d4
ZHENGRUI <38859764@qq.com>
2019-08-09 10:25:27 +0800

    新增通用类

---
+++ PointCloud/Common.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "Common.h"
+
+
+Common::Common()
+{
+}
+
+
+Common::~Common()
+{
+}
+
+
+void Common::PrintString(const string &str)
+{
+
+}
---
+++ PointCloud/Common.h
@@ -0,0 +1,11 @@
+#pragma once
+class Common
+{
+public:
+	Common();
+	~Common();
+	
+	/**************通用功能函数****************/
+	static void PrintString(const string &str);
+};
+
